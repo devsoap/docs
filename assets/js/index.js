55b557af3f6250fbcb366d8db3075db7e03935c0
John Ahlroos <john@ahlroos.me>
2019-05-11 18:07:33 +0100

    Add code highlighting

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,19 +1,20 @@
 
 function showSubmenu(id) {
 
+    var link = document.getElementById(id);
+    link.classList.add("selected-menu");
+
     var menus = document.getElementsByClassName("root-menu");    
 
-    var currentMenu = id == '' ? menus[0] : document.getElementById('side-nav-' + id);    
+    var currentMenu = id == '' ? menus[0] : document.getElementById('side-nav-' + id);
     while(!currentMenu.classList.contains('root-menu')) {
         currentMenu = currentMenu.parentElement;
     }
 
     currentMenu.parentNode.parentNode.classList.add('open');
-
     for (var i=0; i < menus.length; i++) {
         if(menus[i] !== currentMenu) {
             menus[i].style.display = 'none';
-            menus[i].parentNode.classList.remove('open');
         }
     }
 }
